3d0e6ce7584fd332ba21501f50db2f6c393ba192
yuvraj singh <46413950+y4yuvraj@users.noreply.github.com>
2023-10-31 04:24:17 +0530

    adding src

---
+++ src/app/pages/admin/admin-components/admin-add-movie/admin-add-movie.component.ts
@@ -0,0 +1,66 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AdminServiceService } from 'src/app/services/admin-service/admin-service.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-admin-add-movie',
+  templateUrl: './admin-add-movie.component.html',
+  styleUrls: ['./admin-add-movie.component.css']
+})
+export class AdminAddMovieComponent implements OnInit {
+  public movieForm!: FormGroup;
+
+  constructor(private adminService:AdminServiceService, private _snackBar:MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.movieForm = new FormGroup({
+      movieName: new FormControl('', Validators.required), 
+      theatreName: new FormControl('', Validators.required),
+      price: new FormControl('', Validators.required),
+      totalTickets: new FormControl('', Validators.required),
+      availableTickets: new FormControl('', Validators.required),
+    });
+  }
+
+  formSubmit()
+  {
+    if(this.movieForm.valid)
+    {
+      this.adminService.addMovie(this.movieForm.value).subscribe(
+        (response)=>{
+          console.log(response);
+          Swal.fire('Success', 'Movie Added', 'success')
+          this.clearForm();
+        },
+        (error)=>
+        {
+          if (error instanceof HttpErrorResponse) {
+            const errorMessage = error.error.errorMessage;
+            this._snackBar.open(errorMessage, 'Close', {
+              duration: 2000,
+            });
+          } else {
+            this._snackBar.open('An error occurred.', 'Close', {
+              duration: 2000,
+            });
+          }
+
+        }
+      )
+    }else
+    {
+      this._snackBar.open('Please fill out the form correctly!!', '', {
+        duration: 2000,
+      }); 
+    }
+    
+  }
+
+  clearForm(){
+    this.movieForm.reset(); 
+  }
+
+}
